825fb9ffad7b9fcb8f42685e0f6e9470565e9244
aisapo <koumoriunko@gmail.com>
2025-10-07 22:46:30 +0900

    i

--- teishutubutu.js
+++ teishutubutu.js
@@ -46,13 +46,8 @@ function addTaskToList(task,isSubmitted = false){
             } else{
                 tasklist.appendChild(li);
 
-                taskhistory.sort((a,b)=> new Date(a.deadline)-new Date(b.deadline));
-    
-                tasklist.innerHTML="";
-                submittedList.innerHTML="";
-                taskhistory.forEach(t=>addTaskToList(t));
             }
-       
+        
         });
     
 
@@ -107,7 +102,13 @@ addButton.addEventListener("click",function(){
     taskhistory.push(task);
     localStorage.setItem("tasks",JSON.stringify(taskhistory));
     
-
+    if(tasklist.append){
+    taskhistory.sort((a,b)=> new Date(a.deadline)-new Date(b.deadline));
+    
+    tasklist.innerHTML="";
+    submittedList.innerHTML="";
+    taskhistory.forEach(t=>addTaskToList(t));
+    }
     titleInput.value="";
     subjectInput.value="";
     deadlineInput.value="";
