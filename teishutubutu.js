9b025f49a2a78bd505a56890e90253cefe85e185
aisapo <koumoriunko@gmail.com>
2025-10-07 23:25:33 +0900

    i

--- teishutubutu.js
+++ teishutubutu.js
@@ -58,7 +58,7 @@ function addTaskToList(task,isSubmitted = false){
                 tasklist.appendChild(li);
                 
             }
-        
+        localStorage.setItem("tasks",JSON.stringify(taskhistory));
         });
     
 
